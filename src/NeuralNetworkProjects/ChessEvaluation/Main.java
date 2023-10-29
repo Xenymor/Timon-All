96e40f959948642d247dc77b5cab93b42d553d05
Xenymor <101249409+Xenymor@users.noreply.github.com>
2023-10-29 16:34:49 +0100

    Implemented ChessEvaluator

---
+++ src/NeuralNetworkProjects/ChessEvaluation/Main.java
@@ -0,0 +1,109 @@
+package NeuralNetworkProjects.ChessEvaluation;
+
+import NeuralNetwork.DataPoint;
+import NeuralNetwork.NeuralNetwork;
+import NeuralNetworkProjects.LanguageClassification.LanguageClassificationMain;
+import StandardClasses.MyArrays;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        new Main().run();
+    }
+
+    private void run() throws IOException, ClassNotFoundException {
+        NeuralNetwork trained = trainNeuralNetwork(100_000);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("C:\\Users\\timon\\IdeaProjects\\Timon-All\\src\\NeuralNetworkProjects\\ChessEvaluation\\neuralNetworkChess.nn"));
+        objectOutputStream.writeObject(trained);
+        System.out.println("Finished training");
+    }
+
+    private NeuralNetwork trainNeuralNetwork(int iterations) throws IOException, ClassNotFoundException {
+        DataPoint[] trainingData = loadData();
+        List<DataPoint[]> trainingChunks = LanguageClassificationMain.getChunks(DataPoint[].class, trainingData, 128);
+        int validationDataCount = trainingChunks.size()/10*2;
+        DataPoint[] validationData = trainingChunks.get(0);
+        for (int i = 0; i < validationDataCount; i++) {
+            validationData = MyArrays.concatenate(validationData, trainingChunks.get(0));
+            trainingChunks.remove(0);
+        }
+        trainingChunks.remove(0);
+        trainingChunks.remove(0);
+        NeuralNetwork neuralNetwork = new NeuralNetwork(64, 1048, 500, 50, 1);
+        if (Files.exists(Path.of("C:\\Users\\timon\\IdeaProjects\\Timon-All\\src\\NeuralNetworkProjects\\ChessEvaluation\\neuralNetworkChess.nn"))) {
+            neuralNetwork = (NeuralNetwork) new ObjectInputStream(new FileInputStream("C:\\Users\\timon\\IdeaProjects\\Timon-All\\src\\NeuralNetworkProjects\\ChessEvaluation\\neuralNetworkChess.nn")).readObject();
+            System.out.println("Loaded");
+        }
+        double learnRate = 0.05;
+        int counter = 0;
+        double bestCost = neuralNetwork.getCost(validationData);
+        double lastCost = bestCost;
+        System.out.println(bestCost);
+        long lastTime = System.nanoTime();
+        while (counter < iterations) {
+            if (TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - lastTime) >= 120) {
+                lastTime = System.nanoTime();
+                double cost = neuralNetwork.getCost(validationData);
+                System.out.println(cost + " after " + counter + " learnings");
+                if (cost >= lastCost || cost <= learnRate * 10) {
+                    learnRate *= 0.1;
+                }
+                if (cost < bestCost) {
+                    bestCost = cost;
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("C:\\Users\\timon\\IdeaProjects\\Timon-All\\src\\NeuralNetworkProjects\\ChessEvaluation\\neuralNetworkChess.nn"));
+                    objectOutputStream.writeObject(neuralNetwork);
+                }
+                lastCost = cost;
+            }
+            neuralNetwork.learn(trainingChunks.get(counter % (trainingChunks.size())), learnRate);
+            counter++;
+        }
+        return neuralNetwork;
+    }
+
+    private DataPoint[] loadData() throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("C:\\Users\\timon\\IdeaProjects\\Timon-All\\src\\NeuralNetworkProjects\\ChessEvaluation\\datasetNeuralNetwork.csv"));
+        EvaluationData[] evaluationData = new EvaluationData[lines.size()];
+        for (int i = 0; i < evaluationData.length; i++) {
+            evaluationData[i] = getEvaluationDatum(lines.get(i));
+        }
+        return evaluationData;
+    }
+
+    private EvaluationData getEvaluationDatum(final String line) {
+        String[] argumentStrings = line.split(",");
+        double[] arguments = new double[argumentStrings.length - 1];
+        double output = Integer.parseInt(argumentStrings[0])/60_000d + 0.5d;
+        for (int i = 0; i < arguments.length - 1; i++) {
+            arguments[i] = Integer.parseInt(argumentStrings[i + 1]);
+        }
+        return new EvaluationData(arguments, output);
+    }
+
+    private class EvaluationData implements DataPoint {
+
+        private final double[] inputs;
+        private final double[] outputs;
+
+        public EvaluationData(final double[] inputs, final double... outputs) {
+            this.inputs = inputs;
+            this.outputs = outputs;
+        }
+
+        @Override
+        public double[] getInputs() {
+            return inputs;
+        }
+
+        @Override
+        public double[] getExpectedOutputs() {
+            return outputs;
+        }
+    }
+}
