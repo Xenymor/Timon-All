c9f402eb31041e741eb868b6a503fb5381267bf6
Xenymor <101249409+Xenymor@users.noreply.github.com>
2025-12-11 17:20:22 +0100

    Solve Day 10 - Task 2

--- src/Puzzles/AdventOfCode/AOC2025/Day10/Day10Task2.java
+++ src/Puzzles/AdventOfCode/AOC2025/Day10/Day10Task2.java
@@ -1,9 +1,15 @@
 package Puzzles.AdventOfCode.AOC2025.Day10;
 
+import com.google.ortools.Loader;
+import com.google.ortools.linearsolver.MPConstraint;
+import com.google.ortools.linearsolver.MPObjective;
+import com.google.ortools.linearsolver.MPSolver;
+import com.google.ortools.linearsolver.MPVariable;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.List;
 
 public class Day10Task2 {
     static void main() throws IOException {
@@ -18,52 +24,57 @@ public class Day10Task2 {
 
             int[][] moves = parseMoves(line);
 
-            PriorityQueue<State> pq = new PriorityQueue<>();
-            Set<String> visited = new HashSet<>();
-            final State start = new State(0, new int[targetState.length]);
-            pq.add(start);
-            visited.add(Arrays.toString(start.state));
+            result += solve(targetState, moves);
 
-            outer:
-            while (!pq.isEmpty()) {
-                State current = pq.poll();
-
-                if (Arrays.equals(current.state, targetState)) {
-                    result += current.cost;
-                    break;
-                }
-
-                for (int[] move : moves) {
-                    int[] newState = applyMove(current.state, move);
-                    final String key = Arrays.toString(newState);
-                    if (visited.contains(key)) {
-                        continue;
-                    }
-                    boolean isValid = true;
-                    for (int j = 0; j < newState.length; j++) {
-                        if (newState[j] > targetState[j]) {
-                            isValid = false;
-                            break;
-                        }
-                    }
-                    if (!isValid) {
-                        continue;
-                    }
-
-                    if (Arrays.equals(newState, targetState)) {
-                        result += current.cost + 1;
-                        break outer;
-                    }
-
-                    pq.add(new State(current.cost + 1, newState));
-                    visited.add(key);
-                }
-            }
             System.out.println("Current result: " + result);
         }
 
         System.out.println("Result: " + result);
 
+    }
+
+    private static long solve(int[] targetState, int[][] moves) {
+        Loader.loadNativeLibraries();
+
+        MPSolver solver = MPSolver.createSolver("SCIP");
+
+        MPVariable[] numApplies = new MPVariable[moves.length];
+        for (int i = 0; i < numApplies.length; i++) {
+            numApplies[i] = solver.makeIntVar(0.0, Double.POSITIVE_INFINITY, "nm_" + i);
+        }
+
+        for (int i = 0; i < targetState.length; i++) {
+            MPConstraint constraint = solver.makeConstraint(targetState[i], targetState[i], "ct_" + i);
+            for (int j = 0; j < moves.length; j++) {
+                if (containsIndex(moves[j], i)) {
+                    constraint.setCoefficient(numApplies[j], 1);
+                }
+            }
+        }
+
+        MPObjective objective = solver.objective();
+        for (final MPVariable numApply : numApplies) {
+            objective.setCoefficient(numApply, 1);
+        }
+        objective.setMinimization();
+
+        var status = solver.solve();
+
+        if (status != MPSolver.ResultStatus.OPTIMAL) {
+            throw new RuntimeException("No optimal solution found!");
+        }
+
+        long total = 0;
+        for (MPVariable numApply : numApplies) {
+            total += (long) numApply.solutionValue();
+        }
+
+        return total;
+    }
+
+    private static boolean containsIndex(int[] move, int idx) {
+        for (int i : move) if (i == idx) return true;
+        return false;
     }
 
     private static int[] applyMove(final int[] state, final int[] move) {
