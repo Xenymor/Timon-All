86a5f146d018579a82039034eeca6909caac939d
Xenymor <101249409+Xenymor@users.noreply.github.com>
2025-12-11 15:57:30 +0100

    Refactor

--- src/Puzzles/AdventOfCode/AOC2025/Day10/Day10Task1.java
+++ src/Puzzles/AdventOfCode/AOC2025/Day10/Day10Task1.java
@@ -14,8 +14,7 @@ public class Day10Task1 {
 
         long result = 0;
 
-        for (int i = 0; i < lines.size(); i++) {
-            final String line = lines.get(i);
+        for (final String line : lines) {
             int targetState = parseTargetState(line);
 
             int[] moves = parseMoves(line);
